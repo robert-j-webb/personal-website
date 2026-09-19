cf9dab8d984ef044b3d4a5604fa1686edaf9e99e
Robert Webb <me@robertjwebb.com>
2026-09-19 09:53:25 +0200

    Deploy static resume to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://robertjwebb.com',
   output: 'static',
   integrations: [react()],
   devToolbar: { enabled: false },
